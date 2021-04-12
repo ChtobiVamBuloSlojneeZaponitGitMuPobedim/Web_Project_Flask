d797c945389eb648846c6b4ffbb808dd94805675
ChtobiVamBuloSlojneeZaponitGitMuPobedim <76117121+ChtobiVamBuloSlojneeZaponitGitMuPobedim@users.noreply.github.com>
2021-04-12 08:35:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask_ngrok import run_with_ngrok
+#from flask_ngrok import run_with_ngrok
 from flask import Flask, render_template, redirect
-import os
+#import os
 
-from loginform import LoginForm
-from sign_inform import Sign_inForm
+#from loginform import LoginForm
+#from sign_inform import Sign_inForm
 
 app = Flask(__name__)
 #run_with_ngrok(app)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+#app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route("/")
 @app.route('/index')
@@ -27,25 +27,25 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
-@app.route('/sign_in', methods=['GET', 'POST'])
-def sign_in():
-    form = Sign_inForm()
-    if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('sign_in.html', title='Регистрация', form=form)
+#@app.route('/sign_in', methods=['GET', 'POST'])
+#def sign_in():
+    #form = Sign_inForm()
+    #if form.validate_on_submit():
+        #return redirect('/success')
+    #return render_template('sign_in.html', title='Регистрация', form=form)
 
 
-@app.route('/login2', methods=['GET', 'POST'])
-def login2():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('login_2.html', title='Авторизация', form=form)
+#@app.route('/login2', methods=['GET', 'POST'])
+#def login2():
+    #form = LoginForm()
+    #if form.validate_on_submit():
+        #return redirect('/success')
+    #return render_template('login_2.html', title='Авторизация', form=form)
 
 
-@app.route('/success')
-def success():
-    return 'success'
+#@app.route('/success')
+#def success():
+    #return 'success'
 
 
 #if __name__ == '__main__':
